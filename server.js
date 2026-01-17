d49c719d42a831edfd4e194d85bc9f01f33ea4f1
DmitryOsipovAXIS <r0980146@ucll.be>
2026-01-17 22:03:15 +0300

    first

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const port = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  if (req.url === "/healthz") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    return res.end("ok");
+  }
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ app: "myapp", path: req.url }));
+}).listen(port, () => console.log("listening on", port));
